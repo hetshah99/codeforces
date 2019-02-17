e3a4d64ce32fd3ce36b94e5229a3b4d472afc564
Niketan Chandarana <42366580+niketan16@users.noreply.github.com>
2019-02-17 17:10:33 +0530

    Add files via upload

---
+++ beautiful_matrix.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+
+public class beautiful_matrix{
+  
+   public static void main(String arg[]){
+ 
+      Scanner sc = new Scanner(System.in);
+      int i;
+      int b=0;
+      int m=0;
+      int c=0;
+
+      for(i=1;i<6;i++){
+          for(m=1;m<6;m++){
+            b=sc.nextInt();
+                if(b==1)
+                   break;
+                 else; 
+          }  
+             if(b==1)
+                   break;
+                 else; 
+      }
+
+       c=(Math.abs(i-3)+Math.abs(m-3));
+  
+        System.out.print(c);
+  }
+}
